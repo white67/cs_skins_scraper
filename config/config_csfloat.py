bbcadf51b8efd4b28baf5b87bb0ee6681be8ffbc
Kamil Pudzisz <kamilpudzisz59@gmail.com>
2025-03-30 19:14:57 +0200

    Delete crap

--- config/config_csfloat.py
+++
@@ -1,8 +0,0 @@
-CSFLOAT_API_ITEMS_LIMIT = 50
-CSFLOAT_PRICE_MIN = 650 # usd XYZ = X.YZ
-MARKETPLACE_CSFLOAT = "csfloat"
-
-URL_CSFLOAT_NEWEST = f"https://csfloat.com/api/v1/listings?limit={CSFLOAT_API_ITEMS_LIMIT}&sort_by=most_recent&min_price={CSFLOAT_PRICE_MIN}"
-
-def csfloat_sale_link(item_id):
-    return f"https://csfloat.com/item/{item_id}"
