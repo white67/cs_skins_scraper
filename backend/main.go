286d56ecdb33b03d3e6ac7d9e648bc7344ddc96b
Kamil Pudzisz <kamilpudzisz59@gmail.com>
2025-03-31 21:17:17 +0200

    Correct SQL queries, WebSocket support

--- backend/main.go
+++ backend/main.go
@@ -7,20 +7,25 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
+var clients = make(map[*websocket.Conn]bool) // Track connected WebSocket clients
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true }, // Allow all origins
+}
 
 // Listing struct to map to the database
 type Listing struct {
 	ItemName         string   `json:"item_name"`
 	MarketHashName   string   `json:"market_hash_name"`
 	ItemType         string   `json:"item_type"`
-	ItemTypeCategory *string  `json:"item_type_category"` // Change from `string` to `*string`
+	ItemTypeCategory *string  `json:"item_type_category"`
 	DefIndex         *int     `json:"def_index"`
 	PaintIndex       *int     `json:"paint_index"`
 	PaintSeed        *int     `json:"paint_seed"`
@@ -40,6 +45,65 @@ type Listing struct {
 	ListingID        int64    `json:"listing_id"`
 	ListingURL       string   `json:"listing_url"`
 	ListingTimestamp int64    `json:"listing_timestamp"`
+	Marketplace      string   `json:"marketplace"`
+	ID               int64    `json:"id"` // ID field
+}
+
+// WebSocket handler
+func handleConnections(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+	clients[conn] = true
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			delete(clients, conn)
+			break
+		}
+	}
+}
+
+// Broadcast new listings to all connected clients
+func broadcastNewListing(listing Listing) {
+	for client := range clients {
+		err := client.WriteJSON(listing)
+		if err != nil {
+			log.Println("WebSocket error:", err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+// Endpoint to insert a new listing and notify clients
+func insertListings(c *gin.Context) {
+	var newListings []Listing // Expect an array of listings
+	if err := c.ShouldBindJSON(&newListings); err != nil {
+		log.Println("JSON Binding Error:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for _, listing := range newListings {
+		query := `INSERT INTO listings (item_name, market_hash_name, item_type, item_type_category, def_index, paint_index, paint_seed, float_value, icon_url, is_stattrak, is_souvenir, rarity, wear, tradable, trade_ban_days, inspect_link, item_description, item_collection, price, price_currency, listing_id, listing_url, listing_timestamp, marketplace) 
+				  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24) RETURNING id`
+
+		err := db.QueryRow(query, listing.ItemName, listing.MarketHashName, listing.ItemType, listing.ItemTypeCategory, listing.DefIndex, listing.PaintIndex, listing.PaintSeed, listing.FloatValue, listing.IconUrl, listing.IsStatTrak, listing.IsSouvenir, listing.Rarity, listing.Wear, listing.Tradable, listing.TradeBanDays, listing.InspectLink, listing.ItemDescription, listing.ItemCollection, listing.Price, listing.PriceCurrency, listing.ListingID, listing.ListingURL, listing.ListingTimestamp, listing.Marketplace).Scan(&listing.ID)
+
+		if err != nil {
+			log.Println("Database Insert Error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		broadcastNewListing(listing) // Notify clients for each listing
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Listings inserted successfully"})
 }
 
 // Initialize the database connection
@@ -73,7 +137,7 @@ func init() {
 
 // Fetch all listings
 func getListings(c *gin.Context) {
-	rows, err := db.Query("SELECT * FROM listings")
+	rows, err := db.Query("SELECT item_name, market_hash_name, item_type, item_type_category, def_index, paint_index, paint_seed, float_value, icon_url, is_stattrak, is_souvenir, rarity, wear, tradable, trade_ban_days, inspect_link, item_description, item_collection, price, price_currency, listing_url, listing_timestamp, listing_id, marketplace FROM listings ORDER BY listing_timestamp DESC LIMIT 50")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -83,9 +147,9 @@ func getListings(c *gin.Context) {
 	listings := []Listing{}
 	for rows.Next() {
 		var listing Listing
-		var id int // hold the id, even though it's not used
+		// var id int // hold the id, even though it's not used
 		if err := rows.Scan(
-			&id, // Scan the id we don't even use
+			// &id, // Scan the id we don't even use
 			&listing.ItemName,
 			&listing.MarketHashName,
 			&listing.ItemType,
@@ -109,6 +173,7 @@ func getListings(c *gin.Context) {
 			&listing.ListingURL,
 			&listing.ListingTimestamp,
 			&listing.ListingID,
+			&listing.Marketplace,
 		); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -119,36 +184,22 @@ func getListings(c *gin.Context) {
 	c.JSON(http.StatusOK, listings)
 }
 
-// // Insert a new listing into the database
-// func insertListing(c *gin.Context) {
-// 	var newListing Listing
-// 	if err := c.ShouldBindJSON(&newListing); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	// Prepare the SQL query to insert a new listing
-// 	query := `INSERT INTO listings (item_name, market_hash_name, item_type, item_type_category, def_index, paint_index, paint_seed, float_value, icon_url, is_stattrak, is_souvenir, rarity, wear, tradable, trade_ban_days, inspect_link, item_description, item_collection, price, price_currency, listing_url, listing_timestamp)
-// 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22) RETURNING listing_id`
-
-// 	var listingID int64
-// 	err := db.QueryRow(query, newListing.ItemName, newListing.MarketHashName, newListing.ItemType, newListing.Price, newListing.PriceCurrency, newListing.ListingURL, newListing.ListingTimestamp).Scan(&listingID)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	newListing.ListingID = listingID
-// 	c.JSON(http.StatusCreated, newListing)
-// }
-
 // Main function
 func main() {
 	r := gin.Default()
 
+	// WebSocket endpoint
+	r.GET("/ws", func(c *gin.Context) {
+		handleConnections(c)
+	})
+
+	// Enable CORS for the frontend to access the backend API
+	r.Use(cors.Default()) // Use the default CORS policy
+
 	// Define routes
-	r.GET("/listings", getListings)
-	// r.POST("/listings", insertListing)
+	r.GET("/api/listings", getListings)
+
+	r.POST("/api/listings", insertListings) // Endpoint for scraper to send new listings
 
 	// Start the server
 	r.Run(":8080")
