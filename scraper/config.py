018f1f5b787b22b0bd81b2752c14af003d8d6481
Kamil Pudzisz <kamilpudzisz59@gmail.com>
2025-05-15 01:41:33 +0200

    Updated config variables

--- scraper/config.py
+++ scraper/config.py
@@ -1,17 +1,36 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
-load_dotenv()
+def load_csfloat_headers():
+    """
+    Load environment variables from .env file.
+    """
+    load_dotenv(override=True)  # overrides existing variables
+    return os.getenv('CSFLOAT_API_KEY')
+
+STEAM_ICON_URL = "https://steamcommunity-a.akamaihd.net/economy/image/"
+
+SKINS_RARITIES = {
+    1: "Consumer Grade",
+    2: "Industrial Grade",
+    3: "Mil-Spec Grade",
+    4: "Restricted",
+    5: "Classified",
+    6: "Covert",
+    7: "Contraband"
+}
 
 # Scraper configuration - CSFLOAT
 CSFLOAT_HEADERS = {
-    'Authorization': os.getenv('CSFLOAT_API_KEY')
+    'Authorization': load_csfloat_headers()
 }
 CSFLOAT_MARKETPLACE = "CSFLOAT" # Marketplace name
 CSFLOAT_API_URL = "https://csfloat.com/api/v1/listings?limit=50&sort_by=most_recent" # API URL for CSFLOAT newest listings
 CSFLOAT_LISTING_URL = "https://csfloat.com/item/" # Base URL for CSFLOAT listings
 CSFLOAT_LIMIT = 50 # Number of listings to scrape at once (maximum)
-CSFLOAT_SCRAPE_INTERVAL = 44  # seconds
+CSFLOAT_SCRAPE_INTERVAL = 31  # seconds
+CSFLOAT_JSON_KEYWORD = "data"
 
 # Scraper configuration - SKINPORT
 SKINPORT_MARKETPLACE = "SKINPORT" # Marketplace name
@@ -19,6 +38,17 @@ SKINPORT_API_URL = "https://skinport.com/api/browse/730?sort=date&order=desc"
 SKINPORT_LISTING_URL = "https://skinport.com/item/" # Base URL for SKINPORT listings
 SKINPORT_LIMIT = 50 # Number of listings to scrape at once (maximum)
 SKINPORT_SCRAPE_INTERVAL = 21  # seconds
+
+# Scraper configuration - SKINBID
+SKINBID_HEADERS = {
+    'Authorization': os.getenv('SKINBID_API_KEY')
+}
+SKINBID_MARKETPLACE = "SKINBID" # Marketplace name
+SKINBID_API_URL = "https://api.skinbid.com/api/search/auctions?sort=created%23desc&sellType=fixed_price&take=120&skip=0&currency=EUR" # API URL for SKINBID newest listings
+SKINBID_LISTING_URL = "https://skinbid.com/listing/" # Base URL for SKINBID listings
+SKINBID_LIMIT = 120 # Number of listings to scrape at once (maximum)
+SKINBID_SCRAPE_INTERVAL = 26  # seconds
+SKINBID_JSON_KEYWORD = "items"
 
 # Backend API configuration
 BACKEND_API_URL = "http://localhost:8080/api/listings"
